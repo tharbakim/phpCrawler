06d322f94d239b837d9d5c53035b04882d591b5d
Tyler McEnaney <tharbakim@hotmail.com>
2022-05-24 10:45:47 -0700

    Stop stripping phpDoc blocks out

--- web/app/Models/Webpage.php
+++ web/app/Models/Webpage.php
@@ -4,15 +4,64 @@ namespace App\Models;
 
 class Webpage
 {
+    /**
+     * Array of URLs representing images that are present on a page.
+     * @var array
+     */
     private array $images = array();
+
+    /**
+     * Array of URLs representing hyperlinks that do not point to a different host/domain.
+     * @var array
+     */
     private array $internalUrls = array();
+
+    /**
+     * Array of URLs representing hyperlinks that do point to a different host/domain.
+     * @var array
+     */
     private array $externalUrls = array();
+
+    /**
+     * The elapsed time it took to load the webpage.
+     * @var string
+     */
     private string $loadTime;
+
+    /**
+     * The elapsed time it took to scan and analyse the webpage.
+     * @var string
+     */
     private string $parseTime;
+
+    /**
+     * The number of nodes scanned when analysing the webpage.
+     * @var int
+     */
     private int $nodesScanned = 0;
+
+    /**
+     * The number of words encountered in the webpage.
+     * @var int
+     */
     private int $wordCount = 0;
+
+    /**
+     * The title of the webpage once loaded.
+     * @var string
+     */
     private string $pageTitle = '';
+
+    /**
+     * The HTTP respone code for the loaded URL.
+     * @var int
+     */
     private int $httpResponse;
+
+    /**
+     * The URL loaded for, and represented by this data model.
+     * @var string
+     */
     private string $pageUrl;
 
     public function __construct($url)
@@ -20,11 +69,19 @@ class Webpage
         $this->pageUrl = $url;
     }
 
+    /**
+     * Returns JSON representation of the data model.
+     * @return string 
+     */
     public function __toString(): string
     {
         return json_encode($this->getResult());
     }
 
+    /**
+     * Provides access to an array of properties 
+     * @return array 
+     */
     public function getResult(): array
     {
         return array(
@@ -41,96 +98,179 @@ class Webpage
         );
     }
 
+    /**
+     * Appends a string to the list of image URLs encountered.
+     * @param string $url 
+     * @return void 
+     */
     public function addImage(string $url): void
     {
         array_push($this->images, $url);
     }
 
+    /**
+     * Getter for the list of image URLs encountered.
+     * @return array 
+     */
     public function getImages(): array
     {
         return $this->images;
     }
 
+    /**
+     * Appends a string to the list of internal URLs encountered.
+     * @param string $url 
+     * @return void 
+     */
     public function addInternalUrl(string $url): void
     {
         array_push($this->internalUrls, $url);
     }
 
+    /**
+     * Getter for the list of internal URLs encountered.
+     * @return array 
+     */
     public function getInternalUrls(): array
     {
         return $this->internalUrls;
     }
 
+    /**
+     * Append a string to the list of external URLs encountered.
+     * @param string $url 
+     * @return void 
+     */
     public function addExternalUrl(string $url): void
     {
         array_push($this->externalUrls, $url);
     }
 
+    /**
+     * Getter for the list of external URLs encountered.
+     * @return array 
+     */
     public function getExternalUrls(): array
     {
         return $this->externalUrls;
     }
 
+    /**
+     * Getter for the page load time.
+     * @return string 
+     */
     public function getLoadTime(): string
     {
         return $this->loadTime;
     }
 
+    /**
+     * Setter for the page load time.
+     * @param string $time 
+     * @return void 
+     */
     public function setLoadTime(string $time): void
     {
         $this->loadTime = $time;
     }
 
+    /**
+     * Getter for the page parse time.
+     * @return string 
+     */
     public function getParseTime(): string
     {
         return $this->parseTime;
     }
 
+    /**
+     * Setter for the page parse time.
+     * @param string $time 
+     * @return void 
+     */
     public function setParseTime(string $time): void
     {
         $this->parseTime = $time;
     }
 
+    /**
+     * Increment the count of nodes scanned while analysing the website represented by this data model.
+     */
     public function addNodeScanned(): void
     {
         $this->nodesScanned++;
     }
 
+    /**
+     * Getter for the count of nodes scanned.
+     * @return int 
+     */
     public function getNodesScanned(): int
     {
         return $this->nodesScanned;
     }
 
+    /**
+     * Add to the ongoing word count for the website represented by this data model.
+     * @param int $words The number of words to add to the count. Defaults to 1.
+     * @return void 
+     */
     public function addWordCount(int $words = 1): void
     {
         $this->wordCount += $words;
     }
 
+    /**
+     * Getter for the page word count.
+     * @return int 
+     */
     public function getWordCount(): int
     {
         return $this->wordCount;
     }
 
+    /**
+     * Getter for the page title.
+     * @return string 
+     */
     public function getPageTitle(): string
     {
         return $this->pageTitle;
     }
 
+    /**
+     * Setter for the page title.
+     * @param string $title 
+     * @return void 
+     */
     public function setPageTitle(string $title): void
     {
         $this->pageTitle = $title;
     }
 
+    /**
+     * Getter for the HTTP response code.
+     * @return int 
+     */
     public function getHttpResponse(): int
     {
         return $this->httpResponse;
     }
 
+    /**
+     * Setter for the HTTP response code.
+     * @param int $code 
+     * @return void 
+     */
     public function setHttpResponse(int $code): void
     {
         $this->httpResponse = $code;
     }
 
+    /**
+     * Getter for the page URL
+     * @return string 
+     */
     public function getPageUrl(): string
     {
         return $this->pageUrl;
--- web/app/Processor/WebCrawler.php
+++ web/app/Processor/WebCrawler.php
@@ -5,9 +5,21 @@ namespace App\Processor;
 use App\Exceptions\InvalidUrlException;
 use Illuminate\Support\Facades\Http;
 use App\Models\Webpage;
+use DOMNode;
 
+/**
+ * Static web crawling procedures to generate an array of \App\Models\Webpage objects representing the
+ * calculated results of analysing a webpage.
+ * @package App\Processor
+ */
 class WebCrawler
 {
+    /**
+     * The entry point for performing a "crawl" and analysis of one or more URLs.
+     * @param string $seedUrl The initial URL to scan and analyse
+     * @param int $depth The maximum number of additional pages to crawl, based on links contained in $seedUrl that refer to the same host.
+     * @return Webpage[]
+     */
     public static function crawl(string $seedUrl, int $depth): array
     {
 
@@ -32,6 +44,16 @@ class WebCrawler
             return array("status" => false, "message" => "An unexpected error occurred.");
         }
     }
+
+    /**
+     * Recursive procedure for scanning additional pages once the seed URL has been processed.
+     * @param mixed $webpages array of result values from scanned webpages
+     * @param mixed $urlPool array of URLs that have been found while scanning previous pages, and are therefore candidates for additional scans
+     * @param mixed $scannedPages array of URLs that have already been scanned
+     * @param mixed $depth Number of pages yet to be scanned before reaching the maximum depth value
+     * @param mixed $pageHashes Array of hashes calculated based on the body of the response for a URL. This is used to avoid scanning a single page multiple times where there are multiple valid URLs pointing to it.
+     * @return mixed 
+     */
     private static function depthScanning(&$webpages, &$urlPool, &$scannedPages, $depth, &$pageHashes)
     {
         while ($depth > 0) {
@@ -58,11 +80,24 @@ class WebCrawler
         return $webpages;
     }
 
+    /**
+     * Function for determining if a URL is valid.
+     * @param mixed $url URL to check the validity of
+     * @return bool 
+     */
     private static function isValidUrl($url)
     {
         return (filter_var($url, FILTER_VALIDATE_URL) !== false);
     }
 
+    /**
+     * Performs the network request and validates that the page is both a valid webpage, and has not already been encountered based on a hash of the response.
+     * @param string|array $url The URL to scan
+     * @param array $pageHashes AN array of previously-encountered hashes based on the response.
+     * @return Webpage A Webpage data object.
+     * @throws InvalidUrlException When the URL fails the validity check
+     * @throws InvalidUrlException  When the URL has already been scanned
+     */
     private static function request(string|array $url, array &$pageHashes): WEbpage
     {
         if (!self::isValidUrl($url)) {
@@ -94,6 +129,14 @@ class WebCrawler
         }
     }
 
+    /**
+     * Helper function to check for an attribute in a \DOMNode object.
+     * 
+     * This is necessary as the \DOMNode object stores the attributes as an indexed array of key => value pairs.
+     * @param DOMNode $node The node to scan for an existing attribute,
+     * @param string $name THe name of the attribute to be scanned for.
+     * @return null|string Either the value of the attribute of the \DOMNode object, or null if it is not found/set.
+     */
     private static function checkForAttribute(\DOMNode $node, string $name): ?string
     {
         if ($node->hasAttributes()) {
@@ -105,10 +148,17 @@ class WebCrawler
         }
         return null;
     }
+
+    /**
+     * Helper function to scan the contents of a request from Laravel's Http class and update a App\Models\Website data model instance with the results.
+     * @param Webpage $webpage The Webpage object to update with the analysed results.
+     * @param object $request The Http request to analyse the response of.
+     * @return Webpage The updated Webpage data model object
+     */
     private static function parseWebsite(Webpage $webpage, object $request): Webpage
     {
 
-        //Prevent throwing exceptions when you come across modern HTML tags
+        //Prevent throwing exceptions when encountering modern HTML tags
         libxml_use_internal_errors(true);
         $startTime = microtime(true);
         $dom = new \DOMDocument();
@@ -116,6 +166,7 @@ class WebCrawler
 
         $parsedUrl = parse_url($webpage->getPageUrl());
 
+        // Attempting to do the entire analysis in one pass of the response body.
         $nodes = $dom->getElementsByTagName('*');
         foreach ($nodes as $node) {
             $webpage->addNodeScanned();
@@ -148,13 +199,19 @@ class WebCrawler
                         } else {
                             $webpage->addExternalUrl($url);
                         }
+                        /**
+                         * Intentionally no `break;` statement here as an `a` element can still contain a `#text` node
+                         * which should count towards the word count of the page.
+                         */
                     }
                 default:
                     $children = $node->childNodes->length;
                     for ($i = 0; $i < $children; $i++) {
                         if ($node->childNodes->item($i)->nodeType === 3) {
                             $text = $node->childNodes->item($i)->textContent;
+                            // Strip away common punctuation
                             $text = str_replace(array(',', '.', '!', '?', ' / '), '', $text);
+                            // Compress whitespace to avoid empty "words" being counted
                             $text = preg_replace('/\s+/', ' ', $text);
                             $webpage->addWordCount(count(array_filter(explode(' ', $text))));
                         }
